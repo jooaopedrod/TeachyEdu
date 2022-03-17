39638364c02e152821a0eb1c559eab03efbe4b2e
Jefferson Chaves <jeffersonchaves@users.noreply.github.com>
2022-03-17 18:06:39 -0300

    Add correct scripts for the proposed carrousel

--- public/javascript/owlCarousel.js
+++ public/javascript/owlCarousel.js
@@ -1,52 +1,59 @@
 $(document).ready(function () {
-    $(".single-testimonial-area").owlCarousel({
-        loop: true,
-        items: 5,
-        center: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            360: {
-                items: 3,
-            },
-            1000: {
-                items: 5,
-            },
-        },
-        autoplay: false,
-        autoplayTimeout: 15000,
-    });
 
-    var carousel = $(".single-testimonial-area"); // Activator Class
-    carousel.owlCarousel({
-        loop: true,
-        items: 3,
-        margin: 0,
-        nav: true,
-        dots: false,
+    var carousel = $('.owl-carousel').owlCarousel({
+        // autoplay : true,
+        loop   : true,
+        margin : 0,
+        nav    : true,
+        dots   : false,
+        center : true,
+        
+        responsive: {
+
+            0:{
+                items:1
+            },
+
+            600:{
+                items:5
+            },
+
+            1000:{
+                items:5
+            }
+        }
     });
 
     checkClasses();
+
     carousel.on("translated.owl.carousel", function (event) {
         checkClasses();
     });
 
     function checkClasses() {
+        
         var total = $(".single-testimonial-area .owl-stage .owl-item.active").length; // nested class from activator class
 
-        $(".single-testimonial-area .owl-stage .owl-item").removeClass("firstActiveItem lastActiveItem"); // nested class from activator class and remove first and last class if already added.
+        console.log("total: " + total);
+
+        $(".single-testimonial-area .owl-stage .owl-item").removeClass("firstActiveItem"); 
+        // nested class from activator class and remove first and last class if already added.
+
+        $(".single-testimonial-area .owl-stage .owl-item").removeClass("lastActiveItem"); 
+        // nested class from activator class and remove first and last class if already added.
 
         $(".single-testimonial-area .owl-stage .owl-item.active").each(function (index) {
             // nested class from activator class
-            if (index === 1) {
+            if (index === 0) {
                 // this is the first one
+
                 $(this).addClass("firstActiveItem"); // add class in first item
             }
-            if (index === total - 2 && total > 1) {
+            if (index === total - 1 && total > 1) {
                 // this is the last one
                 $(this).addClass("lastActiveItem"); // add class in last item
             }
         });
-    }
+    };
+
 });
